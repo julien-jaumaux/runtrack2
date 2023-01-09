6929078e73d8f8a6ebfafde2f815c6061577b666
jaumaux julien <julien.jaumaux@laplateforme.io>
2023-01-09 12:29:48 +0100

    commit fin job 02 sur commentaire php

---
+++ jour01/job02/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /*voici la synthaxe pour écrire un commentiare sur plusieurs lignes*/
+    echo "Hello LaPlateforme!";?><br>
+    
+    <?php
+    //ceci sert à commenter une seule ligne
+    echo "Hello LaPlateforme!";?>
+
+
+</body>
+</html>
