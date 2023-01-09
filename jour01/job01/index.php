a29909287c86e44e0ab0419fc4e1b938a4c6dcd8
jaumaux julien <julien.jaumaux@laplateforme.io>
2023-01-09 12:15:20 +0100

    commit fin job 02

---
+++ jour01/job01/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $str = "LaPlateforme ";
+    $str2 = "Vive ";
+    $str3 ="!";
+    echo $str2.$str.$str3;?><br>
+    <?php
+    $val = 6;
+    echo $val;?><br>
+    <?php 
+    echo ($val + 4);?><br> 
+    <?php
+    $myBool = true;
+    echo $myBool;
+    /*1 s'affiche pour la valeur true*/
+    ?><br>
+    <?php
+    $myBool = false;
+    echo $myBool;
+    /*rien ne s'affiche pour la valeur false*/
+    ?><br>
+    
+    
+
+</body>
+</html>
