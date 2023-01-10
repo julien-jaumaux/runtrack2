b219bccf6d6114f61918d146bd1e42c4655c8756
jaumaux julien <julien.jaumaux@laplateforme.io>
2023-01-10 13:08:00 +0100

    commit job 6 condition

---
+++ jour02/job06/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>algorythme rectangle</title>
+</head>
+<body>
+    <?php
+        $lr=20;
+        $h=10;
+    ?>
+    <div style="width:<?php echo $lr?>px; height:<?php echo $h ?>px; border: 1px solid black;">
+    
+    </div>
+</body>
+</html>
