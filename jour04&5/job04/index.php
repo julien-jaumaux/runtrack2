d972b15a74e7551f7f05f8bd85f520a2539517b0
jaumaux julien <julien.jaumaux@laplateforme.io>
2023-01-12 14:05:54 +0100

    commit job 4 post php

---
+++ jour04&5/job04/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tableau post</title>
+</head>
+<body>
+<form action="" method="post" class="formCol">
+        <label for="nom">Qel est votre nom:</label>
+        <input type="text" name="nom" id="nom"><br>
+
+        <label for="prenom">Quel est votre prenom:</label>
+        <input type="text" name="prenom" id="prenom"><br>
+
+        <label for="age">Quel est votre age:</label>
+        <input type="text" name="age" id="age"><br>
+
+        <input type="submit" value="Envoyer">
+    </form>
+
+    <?php
+            echo "<table border=2><tr><th>argument</th><th>valeur</th></tr>";
+
+    foreach($_POST as $key => $value){         
+        
+        echo "<tr><td>$key</td><td>$value</td></tr>";
+    }
+    echo "</table>";
+
+    ?>
+
+</body>
+</html>
