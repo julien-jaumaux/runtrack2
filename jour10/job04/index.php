c8451c8fad64f77d43b44dcaec198d8db5e841e6
jaumaux julien <julien.jaumaux@laplateforme.io>
2023-01-20 11:37:13 +0100

    commit jour10/job04/index.php

---
+++ jour10/job04/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $mysqli = new mysqli("localhost", "root", "", "jour009");
+    if ($mysqli -> connect_errno){
+        echo "failed to connect to my MySQL" .$mysqli -> connect_error; 
+        exit();
+    }
+    $request = $mysqli->query("SELECT * FROM `etudiants` WHERE LEFT(prenom, 1) ='T'");
+    $results = $request->fetch_array(MYSQLI_ASSOC);
+
+    echo "<table border='1'><tr>";
+    
+    
+    foreach ($results as $key => $value)
+        {
+        echo " <th> " . $key . " </th> ";
+        }
+        echo "</tr>";
+        while ($results != NULL)
+        {
+        echo "<tr>";
+        foreach ($results as $value)
+        {
+                echo "<td>" . $value . "</td>";
+        }
+        echo "</tr>";
+        $results = $request -> fetch_array(MYSQLI_ASSOC);
+    }
+    echo "</table>";
+    ?>
+</body>
+</html> 
